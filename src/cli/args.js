33da84520fdbbf7c1a18d0863ca3b11d11933500
ElemartA <info88881@gmail.com>
2022-06-05 00:11:53 +0300

    feat: add solve

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const argum = process.argv.slice(2);
+  console.log(process.argv);
+  console.log(argum);
+
+  const result = [];
+
+  for (let i = 0; i < argum.length; i += 2) {
+    result.push(`${argum[i].slice(2)} is ${argum[i + 1]}`);
+  }
+  console.log(result.join(", "));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+process.env.RSS_name1 = "value1";
+process.env.RSS_name2 = "value2";
+process.env.RSS_name3 = "value3";
+
 export const parseEnv = () => {
-    // Write your code here 
+  const variables = process.env;
+  console.log(
+    Object.keys(variables)
+      .filter((el) => el.slice(0, 4) === "RSS_")
+      .map((elem) => `${elem} = ${variables[elem]}`)
+      .join(",")
+  );
 };
+
+parseEnv();
