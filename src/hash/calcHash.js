356bbed8e69ef1d0b9f58d221d9707a0190b25d9
ElemartA <info88881@gmail.com>
2022-06-05 21:50:00 +0300

    feat: add solutions: hash streams zip modules

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        throw new Error("FS operation failed");
+      } else {
+        const hash = crypto.createHash("sha256").update(data).digest("hex");
+        console.log(hash);
+      }
+    }
+  );
 };
+
+calculateHash();
